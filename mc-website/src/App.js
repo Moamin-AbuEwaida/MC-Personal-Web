28bbbb4c79b30ec0affd682c15259723f64f7f32
Moamin-AbuEwaida <moamin.salamah@gmail.com>
2022-02-10 17:40:26 +0100

    working app

---
+++ mc-website/src/App.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import React, { Component, useState, useContext } from 'react'
+import './App.css'
+import Intro from './components/introduction/intro'
+import About from './components/about/about'
+import PorjectList from './components/projectList/projectList'
+import Contact from './components/contact/contact'
+import Toggle from './components/toggle/toggle'
+import { ThemeContext, ThemeProvider } from './context'
+
+
+const App = ()=>{
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+
+  return(
+    <div style={{ backgroundColor:darkMode ? "#222" : "white", color: darkMode && "white", }}>
+      <Toggle />
+      <Intro />
+      <About />
+      <PorjectList />
+      <Contact />
+    </div>
+  )
+}
+/*
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <intro />
+      </div>
+    );
+  }
+}
+*/
+
+export default App;
